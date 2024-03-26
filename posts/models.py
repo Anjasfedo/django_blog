567cddddf1025750b6333dd33e5c5efae1db190e
AnjasFedo <fedoaf98@gmail.com>
2024-03-26 14:50:14 +0700

    Post Model & Migration

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField(max_length=100000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    
+    return render(request, 'index.html', context)
